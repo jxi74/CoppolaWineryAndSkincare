f9b5bf1adcc373553c54f0e018944e62d28efd24
jxi74 <jason.isely@gmail.com>
2024-04-12 01:39:14 -0400

    Work done on winery page

---
+++ wp-content/uploads/uag-plugin/assets/2000/uag-js-1939.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"67e05ed7","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-67e05ed7' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"67e05ed7","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-67e05ed7' );
+});
+ });
