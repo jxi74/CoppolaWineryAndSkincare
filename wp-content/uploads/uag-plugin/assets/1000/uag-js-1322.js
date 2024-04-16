f5fd1643bd03ff723f658bbbef4f068d563bc96e
jxi74 <jason.isely@gmail.com>
2024-04-16 01:00:23 -0400

    Fixed news section and page

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-1322.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1322.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
 	var swiper = new Swiper( ".uagb-block-10072ae8 .uagb-swiper",
-		{"autoplay":{"delay":5214,"disableOnInteraction":false,"pauseOnMouseEnter":false,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":false,"navigation":false}	);
+		{"autoplay":{"delay":5214,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":false,"navigation":false}	);
 });
 
 window.addEventListener("DOMContentLoaded", function(){
 	var swiper = new Swiper( ".uagb-block-3cce32c8 .uagb-swiper",
-		{"autoplay":{"delay":5214,"disableOnInteraction":false,"pauseOnMouseEnter":false,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":false,"navigation":false}	);
+		{"autoplay":{"delay":5214,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":false,"navigation":false}	);
 });
 
  });
