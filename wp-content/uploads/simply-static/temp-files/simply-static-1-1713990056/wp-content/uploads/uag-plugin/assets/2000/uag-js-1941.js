efb1eda91588976af8a760d589689e92e85eaed1
jxi74 <jason.isely@gmail.com>
2024-04-24 16:21:56 -0400

    Deploying

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1713990056/wp-content/uploads/uag-plugin/assets/2000/uag-js-1941.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-be58af23 .uagb-swiper",
+		{"autoplay":{"delay":6001,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":false,"navigation":false}	);
+});
+
+ });
